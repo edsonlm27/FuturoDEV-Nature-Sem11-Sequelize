05798b71de3b44f76793b09bf9d71b4d90a71c5f
Edson Luiz Morais <edsonlm27@gmail.com>
2024-04-20 21:30:00 -0300

    Exercise 3 included and 1 updated

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -27,16 +27,54 @@ routes.get("/alunos", async (req, res) => {
 });
 
 routes.post("/cursos", async (req, res) => {
-  const nome = req.body.nome;
-  const duracao_horas = req.body.duracao_horas;
+  try {
+    const nome = req.body.nome;
+    const duracao_horas = req.body.duracao_horas;
 
-  const curso = await Curso.create(req.body);
-  res.json(curso);
+    if (!nome) {
+      return res.status(400).json({ message: "O nome é obrigatório!" });
+    }
+
+    if (duracao_horas < 50 || duracao_horas > 150) {
+      return res
+        .status(400)
+        .json({ message: "A duração do curso deve ser entre 50 e 150 horas." });
+    }
+
+    const curso = await Curso.create(req.body);
+    res.status(201).json(curso);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Não foi possível cadastrar o curso." });
+  }
 });
 
+// Pesquisa pelo nome de um curso e duração
 routes.get("/cursos", async (req, res) => {
-  const cursos = await Curso.findAll(); // SELECT * from cursos
-  res.json(cursos);
+  try {
+    let params = {};
+
+    // SE for passado uma paramero QUERY chamado "nome" na requisição, então
+    // esse parametro "nome" é adicionado dentro da variavel params
+    if (req.query.nome) {
+      // o ...params, cria uma cópia do params com os chaves e valores já existentes
+      params = { ...params, nome: req.query.nome };
+    }
+
+    if (req.query.duracao_horas) {
+      // o ...params, cria uma cópia do params com os chaves e valores já existentes
+      params = { ...params, duracao_horas: req.query.duracao_horas };
+    }
+
+    const cursos = await Curso.findAll({
+      where: params,
+    });
+
+    res.json(cursos);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: "Não possível listar todos os cursos" });
+  }
 });
 
 module.exports = routes;
